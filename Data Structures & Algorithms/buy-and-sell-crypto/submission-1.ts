2b79003928a0fcac39555a2afbf5ba9e2c11eb1e
Ikhlak Shaikh <ikhlak789@gmail.com>
2026-09-13 23:56:42 -0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let left = 0
+        let right = 1
+        let maxProfit = 0
+        
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                let profit = prices[right] - prices[left]
+                maxProfit = Math.max(profit, maxProfit)
+            } else {
+                left = right
+            }
+            right += 1
+        }
+        return maxProfit
+    }
+}
